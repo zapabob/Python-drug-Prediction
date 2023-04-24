acf06d8cad410c1096a16d614ad81f1efc58db8f
bob <130830376+zapabob@users.noreply.github.com>
2023-04-25 08:41:20 +0900

    Update PythonResearchChemicaldrug.py

--- PythonResearchChemicaldrug.py
+++ PythonResearchChemicaldrug.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
-
+import random
 import pandas as pd
 import tkinter as tk
 import classes as cl
@@ -11,6 +11,29 @@ def get_adme_data(smiles: str) -> pd.DataFrame:
     adme = cl.SwissADME(smiles)
     return adme.get()
 
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
+
+def roll_dice():
+    return random.randint(1, 6)
+
+while True:
+    dice_result = roll_dice()
+    print(f"サイコロの結果: {dice_result}")
+
+    if is_prime(dice_result):
+        break
+
+    wait_time = dice_result * 10
+    print(f"{wait_time}秒待機します...")
+    time.sleep(wait_time)
+
+print("アクセスを再開します。")
 def get_ic50_data(adme_data: pd.DataFrame) -> dict[str, str]:
     # 各モノアミン受容体への半数阻害効果濃度を取得する
     IC50_data = {}

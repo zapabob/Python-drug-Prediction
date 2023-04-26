d19feb0bf5df13a25adb84a64e3eb32d9aa406b5
yasuda0102 <webnuac@gmail.com>
2023-04-26 20:05:35 +0900

    微修正

--- PythonResearchChemicaldrug.py
+++ PythonResearchChemicaldrug.py
@@ -1,31 +1,8 @@
 #!/bin/env python3
-import random
 import pandas as pd
 from classes import MainWindow
 
-def is_prime(number):
-    if number < 2:
-        return False
-    for i in range(2, number):
-        if number % i == 0:
-            return False
-    return True
 
-def roll_dice():
-    return random.randint(1, 6)
-
-while True:
-    dice_result = roll_dice()
-    print(f"サイコロの結果: {dice_result}")
-
-    if is_prime(dice_result):
-        break
-
-    wait_time = dice_result * 10
-    print(f"{wait_time}秒待機します...")
-    time.sleep(wait_time)
-
-print("アクセスを再開します。")
 def get_ic50_data(adme_data: pd.DataFrame) -> dict[str, str]:
     # 各モノアミン受容体への半数阻害効果濃度を取得する
     IC50_data = {}
@@ -34,6 +11,7 @@ def get_ic50_data(adme_data: pd.DataFrame) -> dict[str, str]:
     IC50_data['SERT'] = adme_data.loc[adme_data['Target'] == 'SERT', 'Ki (nM)'].iloc[0]
     return IC50_data
 
+
 def get_ec50_data(adme_data: pd.DataFrame) -> dict[str, str]:
     # 各モノアミン受容体への半数効果濃度を取得する
     ec50_data = {}
@@ -41,6 +19,7 @@ def get_ec50_data(adme_data: pd.DataFrame) -> dict[str, str]:
     ec50_data['NAT'] = adme_data.loc[adme_data['Target'] == 'NET', 'EC50 (nM)'].iloc[0]
     ec50_data['SERT'] = adme_data.loc[adme_data['Target'] == 'SERT', 'EC50 (nM)'].iloc[0]
     return ec50_data
+
 
 # the main function
 # This function has no arguments and returns no values.

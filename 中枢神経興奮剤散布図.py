6a945a1de7c16a5acc1b43679330a20beee84dd9
zapabob <r.minegishi1987@gmail.com>
2023-05-16 20:11:31 +0900

    微修正

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -7,56 +7,34 @@ from sklearn.linear_model import LinearRegression
 # ファイルのリスト
 files = ['MPH.csv','PR.csv','4MMC.csv','4mar.csv','A.csv','Cocaine.csv','MA.csv']
 
-# 各ファイルからデータを読み込み、MWあたりのXLOGPのデータを取得
+# 各ファイルからデータを読み込み、XLOGPとLOG(MW)のデータを取得
 def process_files(files):
-    data = []
+    data_x = []
+    data_y = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
-            XLOGP_per_MW = df['XLOGP'] * df['MW']  # MW*XLOGPを計算
-            data.append(XLOGP_per_MW)
-    return data
+            data_x.append(np.log(df['MW']))  # LOG(MW)を計算
+            data_y.append(df['XLOGP'])
+    return data_x, data_y
 
 # データを散布図で表示し、回帰直線を描く
-def plot_data(data):
+def plot_data(data_x, data_y):
     plt.figure(figsize=(10, 6))
-    x = np.array(range(len(data))).reshape(-1, 1)
-    for i, d in enumerate(data):
-        plt.scatter([i+1]*len(d), d, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
-        reg = LinearRegression().fit(x, d.values.reshape(-1, 1))
+    for i in range(len(data_x)):
+        x = data_x[i].values.reshape(-1, 1)
+        y = data_y[i].values.reshape(-1, 1)
+        plt.scatter(x, y, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
+        reg = LinearRegression().fit(x, y)
         plt.plot(x, reg.predict(x), color='red')
-    plt.xlabel('File')
-    plt.ylabel('XLOGP * MW')
-    plt.xticks(range(1, len(files)+1), [f.split('.')[0] for f in files])
-    plt.xlim([0, len(files)+1])  # x軸の表示範囲を設定
-    plt.ylim([min(min(data)), max(max(data))])  # y軸の表示範囲を設定
-    plt.legend()
-    plt.show()
-
-def main():
-    data = process_files(files)
-    plot_data(data)
-
-if __name__ == "__main__":
-    main()
-
-
-# データを散布図で表示し、回帰直線を描く
-def plot_data(data):
-    plt.figure(figsize=(10, 6))
-    for i, (logMW, XLOGP) in enumerate(data):
-        plt.scatter(logMW, XLOGP, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
-        reg = LinearRegression().fit(logMW.values.reshape(-1, 1), XLOGP.values.reshape(-1, 1))
-        predicted_values = reg.predict(logMW.values.reshape(-1, 1))
-        plt.plot(logMW, predicted_values, color='red')
-    plt.xlabel('LogMW')
+    plt.xlabel('Log(MW)')
     plt.ylabel('XLOGP')
     plt.legend()
     plt.show()
 
 def main():
-    data = process_files(files)
-    plot_data(data)
+    data_x, data_y = process_files(files)
+    plot_data(data_x, data_y)
 
 if __name__ == "__main__":
     main()

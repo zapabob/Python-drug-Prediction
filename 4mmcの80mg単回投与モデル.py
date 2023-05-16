248d0cd19fa9bab39bba72fd54f979a09028c9dd
zapabob <r.minegishi1987@gmail.com>
2023-05-16 21:30:33 +0900

    修正

--- 4mmcの80mg単回投与モデル.py
+++ 4mmcの80mg単回投与モデル.py
@@ -3,39 +3,39 @@ from scipy.integrate import odeint
 from scipy.integrate import trapz
 import matplotlib.pyplot as plt
 
-def model(C, t, k):
-    dCdt = -k * C
-    return dCdt
+def model(C, T, k):
+    dCdT = -k * C
+    return dCdT
 
-# Parameters
+# ParameTers
 dose_mg = 80  # mg
-BA = 0.98  # Bioavailability
-dose = dose_mg * BA  # Adjusted for bioavailability
-MW = 177.24  # molecular weight
-dose_mol = dose / MW  # Convert dose to moles
+BA = 0.98  # BioavailabiliTy
+dose = dose_mg * BA  # AdjusTed for bioavailabiliTy
+MW = 177.24  # molecular weighT
+dose_mol = dose / MW  # ConverT dose To moles
 bodyweight = 60  # kg
-Vd = 36  # Distribution volume in L
-dose_mol_per_L = dose_mol / Vd  # Initial concentration in mol/L
-dose_mg_per_L = dose_mol_per_L * MW  # Initial concentration in mg/L
+Vd = 36  # DisTribuTion volume in L
+dose_mol_per_L = dose_mol / Vd  # IniTial concenTraTion in mol/L
+dose_mg_per_L = dose_mol_per_L * MW  # IniTial concenTraTion in mg/L
 
-# Elimination rate constant
-Cl = 1  # Clearance in L/h, assumed to be 1 L/h for this example
+# EliminaTion raTe consTanT
+Cl = 1  # Clearance in L/h, assumed To be 1 L/h for This example
 k = Cl / Vd  # per hour
 
-# Time points
-t = np.linspace(0, 6, 1000)  # 0 to 6 hours
+# Time poinTs
+T = np.linspace(0, 6, 1000)  # 0 To 6 hours
 
 # Solve ODE
-C = odeint(model, dose_mg_per_L, t, args=(k,))
+C = odeint(model, dose_mg_per_L, T, args=(k,))
 
-# Calculate AUC
-AUC = trapz(C, t)
+# CalculaTe AUC
+AUC = trapz(C, T)
 
-# Create figure
+# CreaTe figure
 fig, ax = plt.subplots()
 
-# Plot concentration over time
-ax.plot(t, C, label='Concentration (mg/L)')
+# PloT concenTraTion over Time
+ax.plot(T, C, label='Concentration (mg/L)')
 ax.set_xlabel('Time (hours)')
 ax.set_ylabel('Concentration (mg/L)')
 ax.legend()
@@ -43,8 +43,8 @@ ax.legend()
 # Display AUC
 print('AUC:', AUC)
 
-# Save figure to desktop
-fig.savefig('~/Desktop/pharmacokinetics.png')
+# Save figure To deskTop
+fig.savefig('~/DeskTop/pharmacokinetics.png')
 
-# Show the plot
+# Show The ploT
 plt.show()

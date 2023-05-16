f57094f6f1f07953888c6ec9ebf96123d90b1415
zapabob <r.minegishi1987@gmail.com>
2023-05-16 19:57:34 +0900

    微修正

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -7,24 +7,26 @@ from sklearn.linear_model import LinearRegression
 # ファイルのリスト
 files = ['MPH.csv','PR.csv','4MMC.csv','4mar.csv','A.csv','Cocaine.csv','MA.csv']
 
-# 各ファイルからデータを読み込み、MWとXLOGPのデータを取得
+# 各ファイルからデータを読み込み、logMWとXLOGPのデータを取得
 def process_files(files):
     data = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
-            data.append((df['MW'], df['XLOGP']))
+            logMW = np.log(df['MW'])  # logMWを計算
+            XLOGP = df['XLOGP']  # XLOGPを取得
+            data.append((logMW, XLOGP))
     return data
 
 # データを散布図で表示し、回帰直線を描く
 def plot_data(data):
     plt.figure(figsize=(10, 6))
-    for i, (x, y) in enumerate(data):
-        plt.scatter(x, y, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
-        x = x.values.reshape(-1, 1)
-        reg = LinearRegression().fit(x, y)
-        plt.plot(x, reg.predict(x), color='red')
-    plt.xlabel('MW')
+    for i, (logMW, XLOGP) in enumerate(data):
+        plt.scatter(logMW, XLOGP, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
+        reg = LinearRegression().fit(logMW.values.reshape(-1, 1), XLOGP.values.reshape(-1, 1))
+        predicted_values = reg.predict(logMW.values.reshape(-1, 1))
+        plt.plot(logMW, predicted_values, color='red')
+    plt.xlabel('LogMW')
     plt.ylabel('XLOGP')
     plt.legend()
     plt.show()

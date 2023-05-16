a1bfe0b6b40ab7a9e04887279b5936217f21f927
zapabob <r.minegishi1987@gmail.com>
2023-05-16 19:34:50 +0900

    微修正

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -1,19 +1,20 @@
 import os
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 # ファイルのリスト
 files = ['MPH.csv','PR.csv','4MMC.csv','4mar.csv','A.csv','Cocaine.csv','MA.csv']
 
-# 各ファイルからデータを読み込み、MWあたりのXLoGPのデータを取得
+# 各ファイルからデータを読み込み、MWあたりのXLOGPのデータを取得
 def process_files(files):
     data = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
-            XLoGP_per_mw = df['XLoGP'] / df['MW']  # MWあたりのXLoGPを計算
-            data.append(XLoGP_per_mw)
+            XLOGP_per_MW = df['XLOGP'] / df['MW']  # MWあたりのXLOGPを計算
+            data.append(XLOGP_per_MW)
     return data
 
 # データを散布図で表示し、回帰直線を描く
@@ -25,7 +26,7 @@ def plot_data(data):
         reg = LinearRegression().fit(x, d.values.reshape(-1, 1))
         plt.plot(x, reg.predict(x), color='red')
     plt.xlabel('File')
-    plt.ylabel('XLoGP per MW')
+    plt.ylabel('XLOGP per MW')
     plt.xticks(range(1, len(files)+1), [f.split('.')[0] for f in files])
     plt.legend()
     plt.show()

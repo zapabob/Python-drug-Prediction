85886f96b357a3affb16a4d462042c998cda01fd
zapabob <r.minegishi1987@gmail.com>
2023-05-16 19:52:55 +0900

    XLOGP/MW

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -7,28 +7,25 @@ from sklearn.linear_model import LinearRegression
 # ファイルのリスト
 files = ['MPH.csv','PR.csv','4MMC.csv','4mar.csv','A.csv','Cocaine.csv','MA.csv']
 
-# 各ファイルからデータを読み込み、MWあたりのXLOGPのデータを取得
+# 各ファイルからデータを読み込み、MWとXLOGPのデータを取得
 def process_files(files):
     data = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
-            XLOGP_per_MW = df['XLOGP'] * df['MW']  # MW*XLOGPを計算
-            data.append(XLOGP_per_MW)
+            data.append((df['MW'], df['XLOGP']))
     return data
 
 # データを散布図で表示し、回帰直線を描く
 def plot_data(data):
     plt.figure(figsize=(10, 6))
-    x = np.array(range(len(data))).reshape(-1, 1)
-    for i, d in enumerate(data):
-        plt.scatter([i+1]*len(d), d, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
-        reg = LinearRegression().fit(x, np.log1p(d.values.reshape(-1, 1)))
-        plt.plot(x, np.exp(reg.predict(x)), color='red')
-    plt.xlabel('File')
-    plt.ylabel('XLOGP * MW')
-    plt.yscale('log')  # Y軸を対数スケールに設定
-    plt.xticks(range(1, len(files)+1), [f.split('.')[0] for f in files])
+    for i, (x, y) in enumerate(data):
+        plt.scatter(x, y, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
+        x = x.values.reshape(-1, 1)
+        reg = LinearRegression().fit(x, y)
+        plt.plot(x, reg.predict(x), color='red')
+    plt.xlabel('MW')
+    plt.ylabel('XLOGP')
     plt.legend()
     plt.show()
 

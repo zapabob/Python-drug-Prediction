bd5c36119a8fd651cace892cbbef496e5e4733e2
zapabob <r.minegishi1987@gmail.com>
2023-05-16 20:28:05 +0900

    両対数グラフ

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -7,31 +7,37 @@ from sklearn.linear_model import LinearRegression
 # ファイルのリスト
 files = ['MPH.csv','PR.csv','4MMC.csv','4mar.csv','A.csv','Cocaine.csv','MA.csv']
 
-# 各ファイルからデータを読み込み、XLOGPとLOG(MW)のデータを取得
+# 各ファイルからデータを読み込み、XLOGPとMWのデータを取得
 def process_files(files):
     data = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
-            df['LOG_MW'] = np.log(df['MW'])
-            data.append(df[['LOG_MW', 'XLOGP']])
+            data.append(df[['MW', 'XLOGP']])
     return data
 
 # データを散布図で表示し、回帰直線を描く
 def plot_data(data):
     plt.figure(figsize=(10, 6))
     for i, df in enumerate(data):
-        x = df['LOG_MW'].values.reshape(-1, 1)
+        x = df['MW'].values.reshape(-1, 1)
         y = df['XLOGP'].values.reshape(-1, 1)
-        plt.scatter(x, y, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
-        reg = LinearRegression().fit(x, y)
-        plt.plot(x, reg.predict(x), color='red')
-    plt.xlabel('Log(MW)')
-    plt.ylabel('XLOGP')
-    plt.xlim([np.min([df['LOG_MW'].min() for df in data]), np.max([df['LOG_MW'].max() for df in data])])  # x軸の表示範囲を設定
-    plt.ylim([np.min([df['XLOGP'].min() for df in data]), np.max([df['XLOGP'].max() for df in data])])  # y軸の表示範囲を設定
+        plt.scatter(np.log10(x), np.log10(y), label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
+        reg = LinearRegression().fit(np.log10(x), np.log10(y))
+        x_line = np.linspace(np.min(np.log10(x)), np.max(np.log10(x)), 100).reshape(-1, 1)
+        plt.plot(x_line, reg.predict(x_line), color='red')
+    plt.xlabel('Log10(MW)')
+    plt.ylabel('Log10(XLOGP)')
     plt.legend()
     plt.show()
+
+def main():
+    data = process_files(files)
+    plot_data(data)
+
+if __name__ == "__main__":
+    main()
+
 
 def main():
     data = process_files(files)

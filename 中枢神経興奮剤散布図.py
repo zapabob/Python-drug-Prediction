94477b47045dee9125f9e8249800e61203c6c170
zapabob <r.minegishi1987@gmail.com>
2023-05-16 18:42:40 +0900

    微修正

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -7,33 +7,33 @@ from sklearn.linear_model import LinearRegression
 # ファイルのリスト
 files = ['MPH.csv','PR.csv','4MMC.csv','4mar.csv','A.csv','Cocaine.csv','MA.csv']
 
-# 各ファイルからデータを読み込み、分子量あたりのMLogPを計算
+# 各ファイルからデータを読み込み、分子量あたりのXLoGPを計算
 def process_files(files):
     MW = []
-    MLogP_values = []
+    XLoGP_values = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
             MW.append(df['MW'])
-            MLogP_values.append(df['MLogP'])
-    return MW, MLogP_values
+            XLoGP_values.append(df['XLoGP'])
+    return MW, XLoGP_values
 
 # データを散布図で表示し、回帰直線を描く
-def plot_data(MW, MLogP_values):
+def plot_data(MW, XLoGP_values):
     plt.figure(figsize=(10, 6))
-    for i, (w, MLogP) in enumerate(zip(MW, MLogP_values)):
-        plt.scatter(w, MLogP, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
-        reg = LinearRegression().fit(np.array(w).reshape(-1, 1), np.array(MLogP).reshape(-1, 1))
-        x = np.linspace(min(w), max(w), 100).reshape(-1, 1)
+    for i, (MW, XLoGP) in enumerate(zip(MW, XLoGP_values)):
+        plt.scatter(MW, XLoGP, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
+        reg = LinearRegression().fit(np.array(MW).reshape(-1, 1), np.array(XLoGP).reshape(-1, 1))
+        x = np.linspace(min(MW), max(MW), 100).reshape(-1, 1)
         plt.plot(x, reg.predict(x), label=f'Regression line {files[i].split(".")[0]}')
-    plt.xlabel('MolWeight')
-    plt.ylabel('MLogP')
+    plt.xlabel('MW')
+    plt.ylabel('XLoGP')
     plt.legend()
-    plt.show()
+    plt.shoMW()
 
 def main():
-    MW, MLogP_values = process_files(files)
-    plot_data(MW, MLogP_values)
+    MW, XLoGP_values = process_files(files)
+    plot_data(MW, XLoGP_values)
 
 if __name__ == "__main__":
     main()

7273daed90cfea7a092f19b486e26926969bf0f6
zapabob <r.minegishi1987@gmail.com>
2023-05-16 23:55:28 +0900

    MDPCYP

--- MDPCYPの1コンパートメントモデルのグラフ.py
+++ MDPCYPの1コンパートメントモデルのグラフ.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import trapz  # AUCを求めるための関数
 
-def one_compartment_model(dose_mg, BA, weight_kg, Vd_L, half_life_h, MW, t_end=24, num_points=1000):
+def one_compartment_model(dose_mg, Vd_L, half_life_h, MW, t_end=24, num_points=1000):
     k_el = np.log(2) / half_life_h  # 排泄定数（h^-1）
 
-    # 投与量をモル単位に変換し、バイオアベイラビリティを考慮
-    dose_mol = dose_mg * BA / MW * 1e-3  # モル単位への変換（mol）
+    # 投与量をモル単位に変換
+    dose_mol = dose_mg / MW * 1e-3  # モル単位への変換（mol）
 
     # 初期濃度（モル/L）
     C0_mol_per_L = dose_mol / Vd_L
@@ -29,19 +29,11 @@ def plot_and_save(t, C_mol_per_L, filename='one_compartment_model.png'):
     plt.savefig(filename)
     plt.show()
 
-def calculate_auc(t, C_mol_per_L):
-    AUC = trapz(C_mol_per_L, t)
-    return AUC
-
 if __name__ == "__main__":
     dose_mg = 15
-    BA = 0.98  # バイオアベイラビリティ
-    weight_kg = 60
     Vd_L = 36
     half_life_h = 4.5
-    MW = 315.41  # 分子量
+    MW = 315.41
     
-    t, C_mol_per_L = one_compartment_model(dose_mg, BA, weight_kg, Vd_L, half_life_h, MW)
-    plot_and_save(t, C_mol_per_L)
-    AUC = calculate_auc(t, C_mol_per_L)
-    print(f"The Area Under the Curve (AUC) is {AUC} mol*h/L")
+    t, C_mol_per_L = one_compartment_model(dose_mg, Vd_L, half_life_h, MW)
+    plot_and_save(t, C_mol_per_L, filename='C:/Users/YourUsername/Desktop/one_compartment_model.png')

a406daf8f8c32de3d518368adcb93bf2755dab6f
zapabob <r.minegishi1987@gmail.com>
2023-05-20 17:42:25 +0900

    文献値を追加

--- 中枢神経興奮剤散布図.py
+++ 中枢神経興奮剤散布図.py
@@ -1,38 +1,35 @@
-from rdkit.Chem import Descriptors
 import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
-from rdkit import Chem
 
-# List of files
+# ファイルのリスト
 files = ['MPH.csv','MA.csv','A.csv']
 
+# 各ファイルからデータを読み込み、TPSAとMWのデータを取得
 def process_files(files):
     data = []
     for file in files:
         if os.path.isfile(file):
             df = pd.read_csv(file)
-            df['Molecule'] = df['SMILES'].apply(Chem.MolFromSmiles)
-            df['MW'] = df['Molecule'].apply(Descriptors.MolWt)
-            df['TPSA'] = df['Molecule'].apply(Descriptors.TPSA)
             data.append(df[['TPSA','MW']])
     return data
 
+# データを散布図で表示し、回帰直線を描く
 def plot_data(data):
     plt.figure(figsize=(10, 6))
     for i, df in enumerate(data):
         x = df['TPSA'].values.reshape(-1, 1)
         y = df['MW'].values.reshape(-1, 1)
-        plt.scatter(x, y, label=files[i].split('.')[0])  # Display data from each CSV file as a scatter plot
-        if len(x) > 1:  # At least two data points are required to draw a regression line
+        plt.scatter(x, y, label=files[i].split('.')[0])  # 各CSVファイルのデータを散布図で表示
+        if len(x) > 1:  # 回帰直線を描くためには少なくとも2つのデータ点が必要です
             reg = LinearRegression().fit(x, y)
             plt.plot(x, reg.predict(x), color='red')
     plt.xlabel('TPSA')
     plt.ylabel('MW')
-    plt.xlim([np.min([df['TPSA'].min() for df in data]), np.max([df['TPSA'].max() for df in data])])  # Set display range for x-axis
-    plt.ylim([np.min([df['MW'].min() for df in data]), np.max([df['MW'].max() for df in data])])  # Set display range for y-axis
+    plt.xlim([np.min([df['TPSA'].min() for df in data]), np.max([df['TPSA'].max() for df in data])])  # x軸の表示範囲を設定
+    plt.ylim([np.min([df['MW'].min() for df in data]), np.max([df['MW'].max() for df in data])])  # y軸の表示範囲を設定
     plt.legend()
     plt.show()
 

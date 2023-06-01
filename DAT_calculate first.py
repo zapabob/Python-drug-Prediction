2d154e74d8f8786947eed60bf0835ae95d58e8f1
zapabob <r.minegishi1987@gmail.com>
2023-06-01 19:08:11 +0900

    カットオフの訂正

--- DAT_calculate first.py
+++ DAT_calculate first.py
@@ -68,7 +68,7 @@ model = Sequential([
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # モデルの訓練
-model.fit(train_data, train_labels, epochs=100, batch_size=64, validation_split=0.2)
+model.fit(train_data, train_labels, epochs=75, batch_size=64, validation_split=0.2)
 
 # モデルの評価
 test_loss = model.evaluate(test_data, test_labels)
@@ -84,8 +84,8 @@ def predict_ic50(iupac_name):
     # モデルによる予測
     predicted_ic50 = model.predict(np.array([descriptors]))
     # IC50が1000を超える場合はN/Aを返す
-    if predicted_ic50 < -np.log10(1000):
-        return "N/A"
+    if predicted_ic50 < 3.000:
+        return "N/A" # IC50 >1000
     else:
         return -np.log10(predicted_ic50)
 
